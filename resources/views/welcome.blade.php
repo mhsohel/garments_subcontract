899cf664e782ef7d8c97c870066193cc3093c6dc
Morshed Habib <mhsohel017@gmail.com>
2022-06-09 16:07:59 +0600

    'final'

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">signup</a>
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">signup   </a>
                         @endif
                     @endauth
                 </div>
