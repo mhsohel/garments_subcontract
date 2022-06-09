3d330971d4864ea948c8be3f3927648569d9d559
jahidjd <jahidjd18@gmail.com>
2022-06-09 17:21:33 +0600

    new test from jahid

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,1 +1,19 @@
-<h1>welcome</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>New</title>
+</head>
+<style>
+    h1{
+        color: red;
+    }
+</style>
+<body>
+    <h1> Test From Jahid</h1>
+
+    
+</body>
+</html>
